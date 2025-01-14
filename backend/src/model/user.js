4346fce3c30f5f4532f5da4bf92a0b24364cdf7c
Pratul Makar <112975329+pratul03@users.noreply.github.com>
2025-01-14 22:47:44 +0530

    Update user.js

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     },
     coordinates: {
       type: [Number], // [longitude, latitude]
-      required: true,
+      required: false,
       validate: {
         validator: function (coordinates) {
           return (
