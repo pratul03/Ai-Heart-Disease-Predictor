a031a76d53b3d1cc08d69ce92c3460fc3d6b0dcc
Pritam Mandal <pritammandal1717@gmail.com>
2025-01-15 20:22:23 +0530

    Dashboard update

--- client/src/components/custom/authDialog.tsx
+++ client/src/components/custom/authDialog.tsx
@@ -11,6 +11,7 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   Dialog,
   DialogContent,
+  DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
@@ -21,26 +22,28 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export function Auth({ label }: { label: string }) {
-
   const navigate = useNavigate();
-  
+
   const [signin, setSignin] = useState({
-    email : "",
-    password : "",
+    email: "",
+    password: "",
   });
 
   const [signup, setSignup] = useState({
-    name : "",
-    email : "",
-    password : "",
+    name: "",
+    email: "",
+    password: "",
   });
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button variant="secondary" className="mr-5">{label}</Button>
+        <Button variant="secondary" className="mr-5">
+          {label}
+        </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] flex justify-center">
+      <DialogContent className="sm:max-w-[425px] flex flex-col items-center justify-center">
+        <DialogTitle>Keep in touch with us 😊</DialogTitle>
         <Tabs defaultValue="account" className="w-[400px]">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="account">SignIn</TabsTrigger>
@@ -57,34 +60,52 @@ export function Auth({ label }: { label: string }) {
               <CardContent className="space-y-2">
                 <div className="space-y-1">
                   <Label htmlFor="username">Username/Email</Label>
-                  <Input id="username" defaultValue="john123@xyz.com" onInput={(e)=> {
-                    setSignin({...signin, email: e.currentTarget.value})
-                  }}/>
+                  <Input
+                    id="username"
+                    defaultValue="john123@xyz.com"
+                    onInput={(e) => {
+                      setSignin({ ...signin, email: e.currentTarget.value });
+                    }}
+                  />
                 </div>
                 <div className="space-y-1">
                   <Label htmlFor="password">Password</Label>
-                  <Input id="password" type="password" onInput={(e) => {
-                    setSignin({...signin, password: e.currentTarget.value})
-                  }} />
+                  <Input
+                    id="password"
+                    type="password"
+                    onInput={(e) => {
+                      setSignin({ ...signin, password: e.currentTarget.value });
+                    }}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button variant={"secondary"} onClick={async() => {
-                  toast.promise(
-                    axios.post("http://localhost:8080/api/auth/signin", signin),
-                    {
-                      loading: "Signing in...",
-                      success: (response) => {
-                        localStorage.setItem("token", response.data.token);
-                        navigate("/dashboard");
-                        return "Signed in successfully!";
-                      },
-                      error: (response) => {
-                        return response.data.message;
-                      },
-                    }
-                  )
-                }}>SignIn</Button>
+                <Button
+                  variant={"secondary"}
+                  onClick={async () => {
+                    toast.promise(
+                      axios.post(
+                        "http://localhost:8080/api/auth/signin",
+                        signin
+                      ),
+                      {
+                        loading: "Signing in...",
+                        success: (response) => {
+                          localStorage.setItem("token", response.data.token);
+                          navigate("/dashboard");
+                          return "Signed in successfully!";
+                        },
+                        error: (response) => {
+                          return response.data
+                            ? response.data.message
+                            : "Internal server error!";
+                        },
+                      }
+                    );
+                  }}
+                >
+                  SignIn
+                </Button>
               </CardFooter>
             </Card>
           </TabsContent>
@@ -99,40 +120,61 @@ export function Auth({ label }: { label: string }) {
               <CardContent className="space-y-2">
                 <div className="space-y-1">
                   <Label htmlFor="name">Name</Label>
-                  <Input id="name" defaultValue={"John Doe"} onInput={(e) => {
-                    setSignup({...signup, name: e.currentTarget.value})
-                  }}/>
+                  <Input
+                    id="name"
+                    defaultValue={"John Doe"}
+                    onInput={(e) => {
+                      setSignup({ ...signup, name: e.currentTarget.value });
+                    }}
+                  />
                 </div>
                 <div className="space-y-1">
                   <Label htmlFor="email">Email</Label>
-                  <Input id="email" type="email" defaultValue={"john123@xyz.com"} onInput={(e) =>{
-                    setSignup({...signup, email: e.currentTarget.value})
-                  }}/>
+                  <Input
+                    id="email"
+                    type="email"
+                    defaultValue={"john123@xyz.com"}
+                    onInput={(e) => {
+                      setSignup({ ...signup, email: e.currentTarget.value });
+                    }}
+                  />
                 </div>
                 <div className="space-y-1">
                   <Label htmlFor="password">Password</Label>
-                  <Input id="password" type="password" onInput={(e) =>{
-                    setSignup({...signup, password: e.currentTarget.value})
-                  }}/>
+                  <Input
+                    id="password"
+                    type="password"
+                    onInput={(e) => {
+                      setSignup({ ...signup, password: e.currentTarget.value });
+                    }}
+                  />
                 </div>
               </CardContent>
               <CardFooter>
-                <Button variant={"secondary"} onClick={async() => {
-                  toast.promise(
-                    axios.post("http://localhost:8080/api/auth/signup", signup),
-                    {
-                      loading: "Signing up...",
-                      success: (response) => {
-                        localStorage.setItem("token", response.data.token);
-                        navigate("/dashboard");
-                        return "Account created successfully!";
-                      },
-                      error: (response) => {
-                        return response.data.message;
-                      },
-                    }
-                  )
-                }}>Sign Up</Button>
+                <Button
+                  variant={"secondary"}
+                  onClick={async () => {
+                    toast.promise(
+                      axios.post(
+                        "http://localhost:8080/api/auth/signup",
+                        signup
+                      ),
+                      {
+                        loading: "Signing up...",
+                        success: (response) => {
+                          localStorage.setItem("token", response.data.token);
+                          navigate("/dashboard");
+                          return "Account created successfully!";
+                        },
+                        error: (response) => {
+                          return response.data.message;
+                        },
+                      }
+                    );
+                  }}
+                >
+                  Sign Up
+                </Button>
               </CardFooter>
             </Card>
           </TabsContent>
