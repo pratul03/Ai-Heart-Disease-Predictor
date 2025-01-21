a812353386daf3da0d2c9b87c1624c197916ecf3
Pritam Mandal <pritammandal1717@gmail.com>
2025-01-22 01:27:07 +0530

    doctor suggestion is added

--- client/src/components/custom/appointment-doctor.tsx
+++ client/src/components/custom/appointment-doctor.tsx
@@ -1,27 +1,7 @@
 function AppointmentDoctor() {
   return (
     <div>
-      <div className="flex flex-col items-center justify-center">
-        <div className="bg-white p-4 rounded-lg shadow-md w-96">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <img
-                src="https://randomuser.me/api/portraits"
-                alt="profile"
-                className="w-12 h-12 rounded-full"
-              />
-              <div className="ml-2">
-                <h2 className="text-lg font-semibold">Dr. John Doe</h2>
-                <p className="text-sm text-gray-400">Dentist</p>
-                <p className="text-sm text-gray-400">10 years experience</p>
-              </div>
-            </div>
-            <button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
-              Book Appointment
-            </button>
-          </div>
-        </div>
-      </div>
+      
     </div>
   );
 }
